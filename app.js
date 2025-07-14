309189d913cdf9a261c734e415ebb630af9818ec
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2025-07-14 22:03:32 +0500

    Refactor addTask function and update HTML button for task submission

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
+// function addTask() {
+//     var newTask = document.createElement('li');
+//     taskList.appendChild(newTask)
+//     newTask.textContent = inputTask.value;
+//     inputTask.value = "";
+// }
+
+
 var taskList = document.getElementById('task-list');
 var inputTask = document.getElementById("inputTask");
 
-function addTask() {
-    var newTask = document.createElement('li');
-    taskList.appendChild(newTask)
-    newTask.textContent = inputTask.value;
-    inputTask.value = "";
-}
+var todos = [];
 
-function deleteTask(newTask) {
+
+function addTask() {
+    event.preventDefault();
+    if (inputTask.value == "") alert("Please write at least 1 todo.");
+    var todo = {
+        id: Date.now(),
+        todo: inputTask.value
+    }
+    todos.push(todo);
+    var newTask = document.createElement(`li`);
+    for (let i = 0; i < todos.length; i++) {
+        newTask.textContent = todos[i].todo
+        taskList.appendChild(newTask);
+    }
+    inputTask.value = "";
+    console.log(todos);
     var deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
+    deleteBtn.textContent = `<span class="material-symbols-outlined">delete</span>`;
     newTask.appendChild(deleteBtn);
     deleteBtn.onclick = function () {
         newTask.remove();
     }
 }
+
+function deleteTask(newTask) {
+
+}
+
+// function edit() {
+
+// }
